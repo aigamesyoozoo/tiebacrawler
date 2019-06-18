dbab6b32c3a7017a0a575c1dffa40c1895e0fd05
Geraldine <geraldine.tayhuiling14@gmail.com>
2019-06-18 11:26:50 +0800

    added venv

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\Documents\5. CODE\PyCharmProjects\BaiduProject\GTDjango\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
